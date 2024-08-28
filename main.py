37fb2474f6e27ab6eae7770b1a8a499fdab69766
1MochaChan1 <danny8doo@gmail.com>
2024-08-28 21:22:46 +0530

    performance optimization, instantiating model at the start

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI
 from mangum import Mangum
-from sentiment_analysis import get_sentiment
+from sentiment_analysis import BertSentimentAnalyzer, get_sentiment
 
 app = FastAPI()
 handler = Mangum(app)
 
+analyzer = BertSentimentAnalyzer()
+analyzer.load_model()
+
 
 @app.get("/l")
 async def get_analysis(link:str):
-    res = get_sentiment(link=link)
+    res = analyzer.get_sentiment(link=link)
     return {"sentiment":res}
     
 
 @app.get("/c")
 async def get_analysis(content:str):
-    res = get_sentiment(content=content)
+    res = analyzer.get_sentiment(content=content)
     return {"sentiment":res}
--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -4,6 +4,50 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification # BER
 import torch # For model-related operations
 import re # For regex
 
+class BertSentimentAnalyzer():
+    def __init__(self) -> None:
+        self.model = None
+        self.tokenizer = None
+
+
+    def load_model(
+        self, 
+        tokenizer:str='nlptown/bert-base-multilingual-uncased-sentiment', 
+        model:str='nlptown/bert-base-multilingual-uncased-sentiment'):
+        
+        # Load pre-trained tokenizer and model
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer)
+        self.model = AutoModelForSequenceClassification.from_pretrained(model)
+        
+    def get_blog_content(self, link: str) -> str:
+        try:
+            res = requests.get(link).text # Fetches page content as text
+            soup = bs4.BeautifulSoup(res) # Parses the HTML content
+            paras = ' '.join([x.text for x in soup.findAll(re.compile("^h[1-6]$"))])
+            # Cleans text by removing tabs, newlines, and commas
+            paras = paras.replace('\t','').replace('\n','').replace(',','')
+            return str(paras).strip() # Returns the cleaned text
+        except Exception as e:
+            print(e) # Prints error if any issues occur
+
+    def get_sentiment(self, link: str=None, content: str=None) -> str:
+        blog_data = None
+        
+        # Get content either from link or passed text
+        if link:
+            blog_data = get_blog_content(link)
+        elif content:
+            blog_data = content
+        
+        tokens = self.tokenizer.encode(blog_data, return_tensors='pt') # Tokenize the content for analysis
+        res = self.model(tokens) # Get sentiment prediction
+        
+        # Determine sentiment: less than 3 is 'NEGATIVE', otherwise 'POSITIVE'
+        verdict = 'NEGATIVE' if int(torch.argmax(res.logits)) + 1 < 3 else 'POSITIVE'
+        
+        return verdict
+    
+    
 def get_blog_content(link: str) -> str:
     try:
         res = requests.get(link).text # Fetches page content as text
